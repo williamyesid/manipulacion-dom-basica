9cec71030814937d5d53c19de720514aee559f8d
William Espinosa <williamyesid22@hotmail.com>
2022-10-21 12:05:28 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+//console.log("Hello, World")
+
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
+
+btn.addEventListener('click', btnOnClick);
+
+function btnOnClick(){
+    const sumaInputs= input1.value + input2.value;
+    pResult.innerHTML="Resultado: " + sumaInputs; 
+}
